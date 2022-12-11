0e5412b04ae0af93e3a7fd1da01bae193c6eaa08
Peter Katz <peter.nichols.katz@gmail.com>
2022-12-11 16:55:18 -0500

    add operate function

--- calc.js
+++ calc.js
@@ -1,15 +1,38 @@
-const add = function(a, b){
+function add(a, b){
     return a + b;
 };
 
-const subtract = function(a, b) {
+function subtract(a, b) {
     return a - b;
 };
 
-const multiply = function(a, b){
+function multiply(a, b){
     return a * b;
 };
 
-const divide = function(a, b){
+function divide(a, b){
     return a / b;
+};
+
+function operate(operator, a, b){
+    a = Number(a)
+    b = Number(b)
+    switch (operator){
+       case '+':
+        return add(a, b)
+        break;
+       case '-':
+        return subtract(a, b)
+        break;
+       case '*':
+        return multiply(a, b)
+        break;
+       case '/':
+        if (b === 0) return null;
+        else return divide(a, b)
+        break;
+       default:
+        return null;
+        break; 
+    }
 };
