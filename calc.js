bd090c79d26f99860c0e59c1ad3e382462799bab
Peter Katz <peter.nichols.katz@gmail.com>
2022-12-11 20:05:45 -0500

    updated HTML, js, and CSS

--- calc.js
+++ calc.js
@@ -1,3 +1,34 @@
+let result = document.getElementById("result");
+const numBtns = document.querySelectorAll(".digit");
+const opBtns = document.querySelectorAll(".operator");
+const clearBtn = document.getElementById("clear");
+
+numBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        let num1 = button.textContent;
+        result.textContent = num1;
+        console.log(button.textContent);
+    })
+});
+
+opBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        let op = button.textContent;
+        result.textContent += " " + op;
+        console.log(button.textContent);
+    })
+});
+
+clearBtn.addEventListener('click', () => {
+    clear();
+});
+
+function clear(){
+    result.textContent = " ";
+};
+
+
+
 function add(a, b){
     return a + b;
 };
